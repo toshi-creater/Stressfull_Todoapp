f295634ed71635fb0aa5d1a0c50f5b7ee6d81aad
梶原利胤 <kajiwararitane@sasakinozominomakku.local>
2025-08-14 16:58:16 +0900

    ログアウトボタンのUIを変更。サイドバーの下に配置。

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 import type { AppDispatch } from "../app/store";
 import { supabase } from "../lib/supabase";
 import { logout } from "../features/auth/authSlice";
+import { Button } from "./ui/button";
 
 const Sidebar = () => {
     const navigate = useNavigate();
@@ -21,22 +22,26 @@ const Sidebar = () => {
             className="fixed top-0 left-0 z-40 w-56 h-screen transition-transform -translate-x-full sm:translate-x-0"
             aria-label="Sidebar"
         >
-            <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-                <a
-                    href="https://flowbite.com/"
-                    className="flex items-center ps-2.5 mb-3"
-                >
-                    <img
-                        src="../logo.png"
-                        className="h-10 me-1"
-                        alt="Flowbite Logo"
-                    />
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                        FLOWDO
-                    </span>
-                </a>
-                <SidebarList />
-                <button onClick={handleLogout}>ログアウト</button>
+            <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800 flex flex-col justify-between">
+                <div>
+                    <a
+                        href="https://flowbite.com/"
+                        className="flex items-center ps-2.5 mb-3"
+                    >
+                        <img
+                            src="../logo.png"
+                            className="h-10 me-1"
+                            alt="Flowbite Logo"
+                        />
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                            FLOWDO
+                        </span>
+                    </a>
+                    <SidebarList />
+                </div>
+                <div className="flex justify-center p-3">
+                    <Button className="w-full text-gray-700 bg-gray-100 hover:bg-gray-200 border border-gray-200" onClick={handleLogout}>ログアウト</Button>
+                </div>
             </div>
         </aside>
     );
