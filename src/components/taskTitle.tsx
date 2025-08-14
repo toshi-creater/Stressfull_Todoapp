ac8c80e61cacfd4db0d921ea60744df039cf6f92
梶原利胤 <kajiwararitane@sasakinozominomakku.local>
2025-08-14 23:17:48 +0900

    タスクのタイトルをサイドバーでクリックしたナビゲーション名になるように修正 #1

---
+++ src/components/taskTitle.tsx
@@ -0,0 +1,15 @@
+import { sidebarConfig } from "@/config/SidebarConfig"
+import { useLocation } from "react-router-dom"
+
+export const TaskTitle = () => {
+    const pathName = useLocation().pathname
+    const nav = sidebarConfig.sidebarNav.find((nav) => nav.href === pathName)
+    
+    return (
+        <div className="pt-5">
+            <h1 className="text-2xl font-semibold text-gray-800 my-4 tracking-tight">
+                {nav ? nav.title : "タスク"}
+            </h1>
+        </div>
+    )
+}
